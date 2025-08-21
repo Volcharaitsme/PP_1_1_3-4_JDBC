e523b2c6d8546489482233086eab9ad31cf76ea3
Daniil Prigozhin <prigozhin@cyberzone.dev>
2025-08-21 20:58:06 +0300

    Adding connenction to DB

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/Local_DB";
+    private static final String USERNAME = "User";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection OK");
+        } catch (SQLException e) {
+            System.out.println("Connection ERROR");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
